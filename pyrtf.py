1a311bcd4d5565ebe8c6f6074f6cdc2d6959c8ce
z3r0zh0u <littlefater2012@gmail.com>
2017-02-17 17:25:21 +0800

    Update code to deal with malformed objdata

--- pyrtf.py
+++ pyrtf.py
@@ -33,12 +33,13 @@ class RTFFile:
         index = 0
         end_index = len(content)
         group_level = -1
+        is_objdata = False
         
         while index < end_index:
             entity = dict()
             entity['offset'] = index
 
-            if content[index] == '\\':
+            if content[index] == '\\' and is_objdata == False:
                 control_type, control_name, control_parameter, control_length = self.__analyze_control(content[index:])
                 if control_name not in ['\'']:
                     entity['type'] = control_type
@@ -48,9 +49,11 @@ class RTFFile:
                     self.entities.append(entity)
                     self.__print_entity(index, entity)
                     index += control_length
+                    if control_name == 'objdata':
+                        is_objdata = True
                     continue
 
-            if content[index] == '{':
+            if content[index] == '{' and is_objdata == False:
                 group_level += 1
                 entity['type'] = 'GroupBegin'
                 entity['content'] = content[index]
@@ -66,10 +69,8 @@ class RTFFile:
                 entity['level'] = group_level
                 index += 1
                 if index < end_index and content[index] == ' ':
-                    entity['content'] += content[index]
                     index += 1
                 elif index+1 < end_index and content[index:index+2] == '\x0d\x0a':
-                    entity['content'] += content[index:index+2]
                     index += 2
                 self.entities.append(entity)
                 self.__print_entity(index, entity)
@@ -86,22 +87,32 @@ class RTFFile:
             i = 0
             entity['type'] = 'Data'
             entity['content'] = ''
-            
-            while index+i < end_index:
-                if content[index+i] == '\\':
-                    if content[index+i+1] in ['{', '}', '\\']:
-                        entity['content'] += content[index+i+1]
-                        i += 2
-                    elif content[index+i+1] == '\'':
-                        entity['content'] += content[index+i+2:index+i+4].decode('hex')
-                        i += 4
-                    else:
+
+            if is_objdata:
+                while index+i < end_index:
+                    if content[index+i] in '0123456789abcdefABCDEF':
+                        entity['content'] += content[index+i]
+                    elif content[index+i] == '}':
                         break
-                elif content[index+i] in ['{', '}']:
-                    break
-                else:
-                    entity['content'] += content[index+i]
                     i += 1
+                        
+                is_objdata = False
+            else:
+                while index+i < end_index:
+                    if content[index+i] == '\\':
+                        if content[index+i+1] in ['{', '}', '\\']:
+                            entity['content'] += content[index+i+1]
+                            i += 2
+                        elif content[index+i+1] == '\'':
+                            entity['content'] += content[index+i+2:index+i+4].decode('hex')
+                            i += 4
+                        else:
+                            break
+                    elif content[index+i] in ['{', '}']:
+                        break
+                    else:
+                        entity['content'] += content[index+i]
+                        i += 1
             
             self.entities.append(entity)
             self.__print_entity(index, entity)
